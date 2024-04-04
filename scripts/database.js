ec4919f7c3370a698eeb8e37784d0d859f62a447
jkhemcha <jkhemcha@ualberta.ca>
2024-04-03 23:41:43 -0600

    Remove duplicate code and extra span tag in html

--- scripts/database.js
+++ scripts/database.js
@@ -2,4 +2,4 @@
 const costDictionary = {"bronze": {bronze: 50, iron: 0, steel: 0, mithril: 0}};
 const baseAttributesDictionary = {"bronze": {damage: 100, critChance: 0.05}};
 const nameDictionary = {"bronze": "Bronze Pickaxe"};
-const oreDictionary = {"bronze": {name: "Bronze Ore", maxHealth: 1000, defense: 100}};
+const oreDictionary = {"bronze": {name: "Bronze Ore", maxHealth: 1000, defense: 100}, "iron": {name: "Iron Ore", maxHealth: 2500, defense: 200}};
--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,6 @@
 // Runs when the page is ready!
 document.addEventListener("DOMContentLoaded", function() {
     let player = new Player();
-    let log = new Log([document.getElementById("logRow1"), 
-                        document.getElementById("logRow2"), 
-                        document.getElementById("logRow3"), 
-                        document.getElementById("logRow4"), 
-                        document.getElementById("logRow5")]);
+    let log = new Log(logDOMElements);
     setupGame(player, log);
 })
