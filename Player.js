b411610f29d2e475cd91af810a3c8ee3504421eb
jkhemcha <jkhemcha@ualberta.ca>
2024-04-03 19:06:09 -0600

    First commit

---
+++ Player.js
@@ -0,0 +1,50 @@
+// Main player class
+class Player {
+    constructor() {
+        this.resources = {
+            bronze: 10,
+            iron: 10,
+            steel: 10,
+            mithril: 10
+        }
+        // Start the player off with a bronze pickaxe when constructed
+        this.pickaxe = createPickaxe("bronze");
+        this.currentResource = "bronze";
+        this.tickRate = 1000;
+    }
+
+    // Method for seeing if you can afford a new pickaxe
+    canAffordPickaxe(type) {
+        if (
+            this.resources.bronze >= costDictionary[type].bronze &&
+            this.resources.iron >= costDictionary[type].iron &&
+            this.resources.steel >= costDictionary[type].steel &&
+            this.resources.mithril >= costDictionary[type].mithril
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    // Method for purchasing a new pickaxe
+    buyPickaxe(type) {
+        if (this.canAffordPickaxe(type)) {
+            this.resources.bronze -= costDictionary[type].bronze;
+            this.resources.iron -= costDictionary[type].iron;
+            this.resources.steel -= costDictionary[type].steel;
+            this.resources.mithril -= costDictionary[type].mithril;
+            this.pickaxe = createPickaxe(type);
+            console.log("New pickaxe bought!");
+            return true;
+        }
+        console.log("Can not afford!");
+        return false;
+    }
+
+    // Method for incrementing a certain resource by an amount
+    mine(type, amount, log) {
+        this.resources[type] += amount;
+        log.write("Mined " + amount + " " + type + "!");
+        renderPlayerData(this);
+    }
+}
---
+++ database.js
@@ -0,0 +1,4 @@
+// Game Database
+const costDictionary = {"bronze": {bronze: 5, iron: 0, steel: 0, mithril: 0}};
+const baseAttributesDictionary = {"bronze": {damage: 100}};
+const nameDictionary = {"bronze": "Bronze Pickaxe"};
