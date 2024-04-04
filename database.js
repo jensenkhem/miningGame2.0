216c2bea534fdb0880a69844ee4baf3ec498ace4
jkhemcha <jkhemcha@ualberta.ca>
2024-04-03 19:49:03 -0600

    We now have a real basic idle game, pickaxe damage doesn't determine anything yet

--- database.js
+++
@@ -1,4 +0,0 @@
-// Game Database
-const costDictionary = {"bronze": {bronze: 5, iron: 0, steel: 0, mithril: 0}};
-const baseAttributesDictionary = {"bronze": {damage: 100}};
-const nameDictionary = {"bronze": "Bronze Pickaxe"};
